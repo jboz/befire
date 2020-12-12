54b11417f8a99092547b42800231a8a186a85063
Julien Boz <julienboz@gmail.com>
2020-12-12 14:56:47 +0100

    try

--- TODO.md
+++ TODO.md
@@ -1,8 +1,10 @@
 # TODO list
 
-- gradle
 - stay connect to a stream with sse
 - listen database modifications
 - créer objet Country
 - impl a rest client
 - continuer HATEOAS
+- gradle
+- exemple KISS
+- exemples d'architecture hexagonale
--- src/main/java/ch/ifocusit/befire/CountriesResource.java
+++ src/main/java/ch/ifocusit/befire/CountriesResource.java
@@ -2,7 +2,8 @@ package ch.ifocusit.befire;
 
 import java.util.HashMap;
 import java.util.Map;
-
+import javax.annotation.PostConstruct;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -12,33 +13,44 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-
 import org.jboss.resteasy.annotations.SseElementType;
-
+import org.reactivestreams.Publisher;
 import io.smallrye.mutiny.Multi;
 
 @Path("/countries")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class CountriesResource {
 
-    private final Map<String, String> countries = new HashMap<>();
+    private final Map<String, String> repository = new HashMap<>();
+
+    private Publisher<String> countries;
+
+    @PostConstruct
+    public void init() {
+        repository.put("France", "France");
+        repository.put("Italie", "Italie");
+
+
+    }
 
     @GET
     @Produces(MediaType.SERVER_SENT_EVENTS)
     @SseElementType(MediaType.APPLICATION_JSON)
     public Multi<String> getAll() {
-        return Multi.createFrom().items("France", "Italie");
+        return Multi.createFrom().publisher(countries);
     }
 
     @GET
     @Path("/{country}")
     public String getCountry(@PathParam("country") String country) {
-        return countries.get(country);
+        return repository.get(country);
     }
 
     @POST
     @Path("/{country}")
     public Response addCountry(@PathParam("country") String country, @Context UriInfo uriInfo) {
-        this.countries.put(country, country);
+        this.repository.put(country, country);
         return Response.created(uriInfo.getAbsolutePathBuilder().build()).build();
     }
 }
